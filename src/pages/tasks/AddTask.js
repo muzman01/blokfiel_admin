3a16c9a0daf0b5403703ae9c5550a10a0eed3b9a
muzman01 <supurge_manq@hotmail.com>
2022-05-27 22:27:07 +0300

    bladmin

---
+++ src/pages/tasks/AddTask.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+function AddTask() {
+  return (
+    <form>
+      <div class="bg-indigo-50 min-h-screen md:px-20 pt-6">
+        <div class=" bg-white rounded-md px-6 py-10 max-w-2xl mx-auto">
+          <h1 class="text-center text-2xl font-bold text-gray-500 mb-10">
+            ADD TASK
+          </h1>
+          <div class="space-y-4">
+            <div>
+              <label for="title" class="text-lx font-serif">
+                Title:
+              </label>
+              <input
+                type="text"
+                placeholder="title"
+                id="title"
+                class="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+              />
+            </div>
+            <div>
+              <label for="description" class="block mb-2 text-lg font-serif">
+                Description:
+              </label>
+              <textarea
+                id="description"
+                cols="30"
+                rows="10"
+                placeholder="whrite here.."
+                class="w-full font-serif  p-4 text-gray-600 bg-indigo-50 outline-none rounded-md"
+              ></textarea>
+            </div>
+            <div>
+              <label for="name" class="form-check-label inline-block text-gray-800">
+                Users:
+              </label>
+              <br></br>
+              <input
+                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+                type="checkbox"
+                value=""
+                id="flexCheckDefault"
+              />
+              <label
+                class="form-check-label inline-block text-gray-800"
+                for="flexCheckDefault"
+              >
+                Default checkbox
+              </label>
+            </div>
+            <div>
+              <label for="email" class="text-lx font-serif">
+                Email:
+              </label>
+              <input
+                type="text"
+                placeholder="name"
+                id="email"
+                class="ml-2 outline-none py-1 px-2 text-md border-2 rounded-md"
+              />
+            </div>
+            <button class=" px-6 py-2 mx-auto block rounded-md text-lg font-semibold text-indigo-100 bg-indigo-600  ">
+              ADD POST
+            </button>
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default AddTask;
